a36777d1562e77e45ec13177906840ce74157686
Hunter Thompson <hunterthompson025@gmail.com>
2024-07-11 18:31:12 -0700

    This commit adds the files and some of the logic to the config, controllers, and utils folders. Also updates the .env file as well as the package.json file and .gitignore file.

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Car, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Get all cars
+
+// Get all Trucks
+
+// Get all SUVs
+
+// Get all Sedans
+
+// Get with middleware to prevent access to route
+
+// Get for login
+
+module.exports = router;
