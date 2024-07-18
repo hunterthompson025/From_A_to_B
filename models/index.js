29a43b0cd312a37432eb22963df70f767b4b2cdc
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-07-17 19:57:22 -0700

    made changes in index.js and car.json files

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require('./User');
 const Car = require('/Car');
+const Category = require('/Category');
+const Comment = require('/Comment');
 
 User.hasMany(Car, {
   foreignKey: 'user_id',
@@ -10,4 +12,31 @@ Car.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = {Car};
+Car.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+Category.hasMany(Car, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Car.hasMany(Comment, {
+  foreignKey: 'car_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Car, {
+  foreignKey: 'car_id'
+});
+
+module.exports = {Car, User, Comment, Category};
