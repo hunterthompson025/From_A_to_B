8f09c44210d716f7c5d534205a168efd7df94b54
Hunter Thompson <hunterthompson025@gmail.com>
2024-07-18 19:23:08 -0700

    This commit removes the category logic for future development, and connects the frontend to the backend.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const commentRoutes = require('./commentRoutes');
-const categoryRoutes = require('./categoryRoutes');
 
 router.use('/users', userRoutes);
-router.use('/comments', commentRoutes);
-router.use('/categories', categoryRoutes);
 
 module.exports = router;
